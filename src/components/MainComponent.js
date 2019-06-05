cbe9673793c7ad813894967b2863bf1cbf418237
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-06-05 08:05:34 -0400

    initial configuration

---
+++ src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
+
+class Main extends Component {
+
+  render(){
+    return(
+      <div>
+        <h1> Cambuche del Mono </h1>
+      </div>
+    );
+
+  }
+
+}
+
+export default Main;
