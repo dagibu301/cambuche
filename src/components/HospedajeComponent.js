72842ac7f5bf2df37c29ba981bbafb2b0381f706
David Giraldo <dagibu007@gmail.com>
2019-06-11 21:09:41 -0400

    hospedaje 2

--- src/components/HospedajeComponent.js
+++ src/components/HospedajeComponent.js
@@ -6,15 +6,45 @@ function Hospedaje() {
       <div>
         <div className="row mt-5">
 
-          <div className="offset-md-1 col-md-2" >
+          <div className="offset-md-1 mr-2 col-md-2 " id="bg-2-lines" >
           </div>
 
-          <div className="col-md-6 ">
+          <div className="col-md-4 mr-lg-5 ml-lg-5">
             <h2 className="yellow-tittle">HOSPEDAJE</h2>
           </div>
 
-          <div className="col-md-2">
+          <div className="col-md-2 ml-2"  id="bg-2-lines">
           </div>
+
+        </div>
+        <div className="row mt-5 justify-content-around">
+
+          <div className="col-sm-12  col-lg-6 col-xl-4 mt-4">
+            <Media object src='/assets/images/cozy-cambuche.png' alt='cozy cambuche' />
+          </div>
+
+          <div className="col-sm-12 col-lg-6 col-xl-4 mt-4 ">
+            <Media object src='/assets/images/chill-cambuche.png' alt='chill cambuche' />
+          </div>
+
+          <div className="col-sm-12 col-lg-6  col-xl-4 mt-4 ">
+            <Media object src='/assets/images/camping.png' alt='camping' />
+          </div>
+
+
+
+          <div className="col-12 col-lg-6 col-sm-12 col-xl-4 mt-4 ">
+            <Media object src='/assets/images/cozy-room.png' alt='cozy room' />
+          </div>
+
+          <div className="col-12 col-lg-6 col-sm-12 col-xl-4 mt-4 ">
+            <Media object src='/assets/images/chill-room.png' alt='chill room' />
+          </div>
+
+          <div className="col-12  col-lg-6 col-sm-12 col-xl-4 mt-4">
+            <Media object src='/assets/images/shared room.png' alt='shared room' />
+          </div>
+
 
         </div>
       </div>
