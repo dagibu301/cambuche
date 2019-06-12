36bee34f1ee89c3b11e7400d3d3e19fa8f92da79
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-06-12 10:59:58 -0400

    hospedaje 3

--- src/components/HospedajeComponent.js
+++ src/components/HospedajeComponent.js
@@ -9,39 +9,46 @@ function Hospedaje() {
           <div className="offset-md-1 mr-2 col-md-2 " id="bg-2-lines" >
           </div>
 
-          <div className="col-md-4 mr-lg-5 ml-lg-5">
+          <div className="col-md-4 mr-lg-5 ml-lg-5 d-flex justify-content-center">
             <h2 className="yellow-tittle">HOSPEDAJE</h2>
           </div>
 
-          <div className="col-md-2 ml-2"  id="bg-2-lines">
+          <div className="col-md-2 ml-2 d-block d-sm-none"  id="bg-2-lines">
+          </div>
+
+
+
+
+
+          <div className="col-md-2 ml-5 d-none d-xl-block"  id="bg-2-lines">
           </div>
 
         </div>
         <div className="row mt-5 justify-content-around">
 
-          <div className="col-sm-12  col-lg-6 col-xl-4 mt-4">
+          <div className="col-sm-12  col-md-6 col-xl-4 mt-4">
             <Media object src='/assets/images/cozy-cambuche.png' alt='cozy cambuche' />
           </div>
 
-          <div className="col-sm-12 col-lg-6 col-xl-4 mt-4 ">
+          <div className="col-sm-12 col-md-6 col-xl-4 mt-4 ">
             <Media object src='/assets/images/chill-cambuche.png' alt='chill cambuche' />
           </div>
 
-          <div className="col-sm-12 col-lg-6  col-xl-4 mt-4 ">
+          <div className="col-sm-12 col-md-6  col-xl-4 mt-4 ">
             <Media object src='/assets/images/camping.png' alt='camping' />
           </div>
 
 
 
-          <div className="col-12 col-lg-6 col-sm-12 col-xl-4 mt-4 ">
+          <div className="col-12 col-md-6 col-sm-12 col-xl-4 mt-4 ">
             <Media object src='/assets/images/cozy-room.png' alt='cozy room' />
           </div>
 
-          <div className="col-12 col-lg-6 col-sm-12 col-xl-4 mt-4 ">
+          <div className="col-12 col-md-6 col-sm-12 col-xl-4 mt-4 ">
             <Media object src='/assets/images/chill-room.png' alt='chill room' />
           </div>
 
-          <div className="col-12  col-lg-6 col-sm-12 col-xl-4 mt-4">
+          <div className="col-12  col-md-6 col-sm-12 col-xl-4 mt-4">
             <Media object src='/assets/images/shared room.png' alt='shared room' />
           </div>
 
