802e9b4e318f5254b7f5587fb6d6d4de9cd619d3
IT dept Valor Homes <itdept@valorhomes100llc.com>
2019-06-20 13:50:04 -0400

    footer update1

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -8,12 +8,12 @@ function Footer() {
 
 
         <div className="row footer-row">
-          <div className="col-12  offset-lg-1 col-lg-5 mt-5">
+          <div className="col-11  offset-1 col-md-5 mt-5">
             <p>CONTACTO</p>
             <p>TEL: 301 672 7804 <br /> IG: ELCAMBUCHEDELMONO <br /> EMAIL: INFO@ELCAMBUCHEDELMONO.COM</p>
             <p>San Antonio-Jamundí, Colombia.</p>
           </div>
-          <div className="col-12   col-lg-6 ">
+          <div className="col-12   col-md-6 d-none d-lg-block ">
             <img className="footer-img" src="/assets/images/montana.png" alt="montanta" />
           </div>
 
